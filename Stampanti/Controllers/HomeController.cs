7fb085b188db2cf8d2662645810bc0b83eeb104a
Pietro Puiatti <pigopuiatti@gmail.com>
2021-01-15 16:08:45 +0100

    Aggiungere i file di progetto.

---
+++ Stampanti/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Stampanti.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stampanti.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+            ElencoStampanti = new List<Stampante>();
+            ElencoStampanti.Add(new Stampante() { Nome = "Boh", IP = "1.1.1.1", Port = 9100 });
+        }
+        public List<Stampante> ElencoStampanti { get; set; }
+
+        public ActionResult Index()
+        {
+            ViewBag.Stampanti = ElencoStampanti;
+            
+            return View();
+        }
+
+        public ActionResult AggiungiStampante()
+        {
+            
+            ElencoStampanti.Add(new Stampante() { Nome = "Boh", IP= "1.1.1.1", Port=9100});
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Aggiungi il nome del reparto: ";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Aggiungi stampante e port: ";
+
+            return View();
+        }
+    }
+}
---
+++ Stampanti/Models/Stampante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stampanti.Models
+{
+    public class Stampante
+    {
+        public string Nome { get; set; }
+        public string IP { get; set; }
+        public int Port { get; set; }
+        
+    }
+}
